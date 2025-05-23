7f67affdb4ac6f04bf9dc38b4d08e7985e526604
Sarthak Makhija <sarthak.makhija@gmail.com>
2025-05-24 00:44:52 +0530

    Sarthak | Adds support for dynamic topics, with dynamic consumers and dynamic group ids

---
+++ src/main/java/com/dqs/configuration/KafkaConsumerInitializer.java
@@ -0,0 +1,22 @@
+package com.dqs.configuration;
+
+import jakarta.annotation.PostConstruct;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+import java.util.List;
+
+@Component
+public class KafkaConsumerInitializer {
+
+    @Autowired
+    private DynamicKafkaConsumerConfiguration dynamicKafkaConsumerConfiguration;
+
+    @PostConstruct
+    public void initializeListeners() {
+        List<String> tenantIds = List.of("jpmc", "deutsche");
+        for (String suffix : tenantIds) {
+            dynamicKafkaConsumerConfiguration.registerConsumerForTopic(suffix);
+        }
+    }
+}
